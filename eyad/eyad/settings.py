fbe2c7c3fe8ba8c5913a5d0784de987ed129283c
eyad <eyadkhg1@gmail.com>
2024-07-09 16:05:21 +0300

    base dir

--- eyad/eyad/settings.py
+++ eyad/eyad/settings.py
@@ -11,9 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 #n
 # Quick-starnt development settings - unsuitable for production
